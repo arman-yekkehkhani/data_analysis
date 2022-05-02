81e8c82835319f71c0a6566ed0f5a197f092f4b7
Arman Yekekhani <yekekhani@viratech.ir>
2022-05-02 19:10:00 +0430

    first commit

---
+++ download_datasets.py
@@ -0,0 +1,56 @@
+""" This module downloads necessary dataset via FTP"""
+import os
+import shutil
+from ftplib import FTP
+from typing import Optional
+
+FTP_HOST = 'ftp.ebi.ac.uk'
+BASE_URL = 'pub/databases/opentargets/platform/21.11/output/etl/parquet/'
+DATASETS_URL_DICT = {
+    'diseases': None,
+    'targets': None,
+    'evidences': 'evidence/sourceId=eva',
+}
+
+
+def fetch(ftp: FTP, name: str, url: Optional[str] = None) -> None:
+    """
+    Fetches datasets from the given url and saves to a directory with specified name
+    """
+    ftp.cwd(BASE_URL + url if url else name)
+    files = ftp.nlst()
+    for idx, f in enumerate(files):
+        print(f'Downloading {name} files: {idx}/{len(files) - 1}')
+        ftp.retrbinary('RETR ' + f, open(f'./datasets/{name}/{f}', 'wb').write)
+
+
+def recreate_dirs() -> None:
+    """
+    Purges existing dataset dir and recreate the structure
+    """
+    # remove old `datasets` dir
+    dataset_dir = './datasets'
+    shutil.rmtree(dataset_dir, ignore_errors=True)
+
+    # create the new dir and sub_dirs
+    for folder in DATASETS_URL_DICT.keys():
+        os.makedirs(os.path.join(dataset_dir, folder))
+
+
+def download_datasets() -> None:
+    """
+    Purges and recreate existing `datasets` dir and downloads datasets
+    """
+    recreate_dirs()
+
+    # make a ftp connection
+    ftp = FTP(FTP_HOST)
+    ftp.login()
+
+    # fetch datasets from the ftp
+    for name, url in DATASETS_URL_DICT.items():
+        fetch(ftp, name, url)
+
+
+if __name__ == "__main__":
+    pass
---
+++ main.py
@@ -0,0 +1,110 @@
+import multiprocessing as mp
+
+import numpy as np
+import pandas as pd
+from pandas import DataFrame
+
+PARQUET_ENGINE = 'pyarrow'
+
+evidence_cols = ['diseaseId', 'targetId', 'score']
+disease_cols = ['id', 'name']
+target_cols = ['id', 'approvedSymbol']
+
+
+def top3(x):
+    """
+    returns top 3 greatest elements
+    """
+    if len(x.values) < 3:
+        return x.values
+    else:
+        idx = np.argpartition(x.values, -3)[-3:]
+        return x.values[idx].tolist()
+
+
+def compute_stats(df: DataFrame) -> DataFrame:
+    """
+    Groups evidence df by ['diseaseId', 'targetId'], then compute median and 3 greatest scores
+    """
+    return (df
+            .groupby(['diseaseId', 'targetId'])
+            .agg({'score': ['median', top3]})
+            .reset_index(level=[0, 1])
+            )
+
+
+def compute_join_stats(evd_df: DataFrame, dss_df: DataFrame, tgt_df: DataFrame) -> DataFrame:
+    """
+    Join evidences, diseases and target dataframes, then sort in ascending order by the median
+    """
+    return (evd_df
+            .merge(dss_df,
+                   left_on="diseaseId",
+                   right_on="id",
+                   how="inner")
+            .merge(tgt_df,
+                   left_on="targetId",
+                   right_on="id",
+                   how="inner")
+            .filter(['diseaseId', 'targetId', 'score', 'name', 'approvedSymbol'])
+            .groupby(['diseaseId', 'targetId'])
+            .agg(median=('score', 'median'))
+            .sort_values(by=['median'])
+            .reset_index(level=[0, 1])
+            )
+
+
+def count_n_common_neighbors(df: DataFrame, n_neigh: int, partitions: int):
+    df = (df
+          .filter(['diseaseId', 'targetId'])
+          .groupby('targetId')
+          .agg(set)
+          )
+
+    # cartesian product
+    df = df.merge(df, how='cross')
+
+    # partition df
+    start_idx = [i * (len(df) // partitions) for i in range(partitions)]
+    end_idx = start_idx[1:]
+    end_idx.append(len(df) - 1)
+    dfs = [df.iloc[s:f, :] for (s, f) in zip(start_idx, end_idx)]
+
+    def f(df: DataFrame):
+        df['has_n_neighbour'] = df.apply(lambda x: len(x['diseaseId_x'] & x['diseaseId_y']) > n_neigh, axis=1)
+        return df
+
+    with mp.Pool(mp.cpu_count()) as pool:
+        res = pd.concat(pool.map(f, dfs))
+
+    # subtract same tgt-tgt count
+    return res['has_n_neighbour'].sum() - df['targetId'].nunique()
+
+
+def spark():
+    # psutils -> get cpus
+    # local[numbr of cpus]
+    pass
+
+
+if __name__ == "__main__":
+    # argparse
+
+    # if over_write or not exists():
+    #   download()
+
+    # load data
+    evd_df = pd.read_parquet('./evidences', engine=PARQUET_ENGINE, columns=evidence_cols)
+    dss_df = pd.read_parquet('./diseases', engine=PARQUET_ENGINE, columns=disease_cols)
+    tgt_df = pd.read_parquet('./targets', engine=PARQUET_ENGINE, columns=target_cols)
+
+    # compute stats of evidences df and save
+    stat_df = compute_stats(evd_df)
+    stat_df.to_json('evidence_stats.json', orient='records', lines=True)
+
+    # compute stats of target-disease df and save
+    dss_tgt_df = compute_join_stats(evd_df, dss_df, tgt_df)
+    dss_tgt_df.to_json('disease_target.json', orient='records', lines=True)
+
+    print(f'Number of target-target pairs share a connection to at least two diseases'
+          f' : {count_n_common_neighbors(evd_df, 2, mp.cpu_count())}')
